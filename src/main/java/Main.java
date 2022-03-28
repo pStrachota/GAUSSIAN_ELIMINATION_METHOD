22e148b8a32b7c8d81fee7293cb4756f51bdef80
pStrachota <p.strachota52@gmail.com>
2022-03-28 19:10:29 +0200

    add main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,45 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import org.javatuples.Pair;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Pair<Integer, List<Double>> integerListPair = null;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe układów równań" +
+                "których chcesz otrzymać rozwiązanie");
+
+        int numberOfEquations = 0;
+        System.out.println("Mozesz wybrac liczbe z przedzialu od 1 do 12");
+        do {
+            String userInput = scanner.nextLine();
+            numberOfEquations = Integer.parseInt(userInput);
+        } while (numberOfEquations > 12 || numberOfEquations <= 0);
+
+        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------");
+
+        for (int i = 1; i <= numberOfEquations; i++) {
+            try {
+                integerListPair = InputReader.read("example_" + i + ".txt");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            Integer n = integerListPair.getValue0();
+            List<Double> doubles = integerListPair.getValue1();
+            List<Double> results = new ArrayList<>();
+            Matrix matrix = new Matrix(n, doubles);
+
+            if (GaussianEliminationMethod.GaussianSolver(matrix, n, results)) {
+                results.forEach(System.out::println);
+            }
+            System.out.println("------------------------------------------------");
+            System.out.println("------------------------------------------------");
+        }
+    }
+}
